5de3da2ad9e82d72c66415656f87e19aa8f5e3cd
Alena Peethala <alenadashi@icloud.com>
2023-08-02 13:22:26 -0700

    Added Lava lamp view file

---
+++ STEM Playground/LavaLamp.swift
@@ -0,0 +1,20 @@
+//
+//  LavaLamp.swift
+//  STEM Playground
+//
+//  Created by Alena Peethala on 8/2/23.
+//
+
+import SwiftUI
+
+struct LavaLamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LavaLamp_Previews: PreviewProvider {
+    static var previews: some View {
+        LavaLamp()
+    }
+}
